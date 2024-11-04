e80b1694ad11a67a6e2351264eac7a082c989cf2
Steffen Kirchhoff <steffen.kirchhoff99@gmx.de>
2024-11-04 13:25:45 +0100

    adjustment to title

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="w-full bg-black h-16 py-2 px-4 flex items-center gap-16">
-      <h1 className="text-white text-2xl font-bold">Dive Spot</h1>
+      <h1 className="text-white text-2xl font-bold">Dive Finder</h1>
       <nav>
         <ul className="flex space-x-4 font-semibold">
           <li>
