a0b9b1a870759bfd865c0ff35a86b29c0d9c5c2f
Steffen Kirchhoff <steffen.kirchhoff99@gmx.de>
2024-11-09 10:52:12 +0100

    fixed mobile details page

--- frontend/app/sites/[siteId]/layout.tsx
+++ frontend/app/sites/[siteId]/layout.tsx
@@ -4,7 +4,7 @@ export default function SitesDetailLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="py-10 px-20 h-full w-full flex justify-center">
+    <div className="py-10 px-4 h-full w-full flex justify-center">
       <div className="max-w-screen-lg w-full flex flex-col min-h-full">
         {children}
       </div>
