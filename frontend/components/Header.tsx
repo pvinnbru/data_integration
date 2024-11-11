3c23131e744da0daf0a578f32dfdc6a42238dd37
Steffen Kirchhoff <steffen.kirchhoff99@gmx.de>
2024-11-11 21:39:42 +0100

    Added auth functionality, login & sighup pages, email verification and confirmation webhook

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,6 +1,14 @@
+import { createClient } from "@/supabase/server";
 import Link from "next/link";
+import UserProfile from "./UserProfile";
 
-const Header = () => {
+const Header = async () => {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   const headerLinks = [
     {
       name: "Home",
@@ -13,24 +21,40 @@ const Header = () => {
   ];
 
   return (
-    <div className="w-full bg-black h-16 py-2 px-4 flex items-center gap-16">
-      <Link
-        href="/"
-        className="text-white text-2xl font-bold hover:opacity-75 transition"
-      >
-        Dive Finder
-      </Link>
-      <nav>
-        <ul className="flex space-x-4 font-semibold">
-          {headerLinks.map((link) => (
-            <li key={link.name}>
-              <Link href={link.href} className="text-white hover:opacity-75">
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <div className="w-full bg-black h-16 py-2 px-4 flex justify-between items-center gap-16">
+      <div className="flex items-center gap-16">
+        <Link
+          href="/"
+          className="text-white text-2xl font-bold hover:opacity-75 transition"
+        >
+          Dive Finder
+        </Link>
+        <nav>
+          <ul className="flex space-x-4 font-semibold">
+            {headerLinks.map((link) => (
+              <li key={link.name}>
+                <Link href={link.href} className="text-white hover:opacity-75">
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className="flex items-center gap-4">
+        {user ? (
+          <UserProfile />
+        ) : (
+          <>
+            <Link href="/login" className="text-white hover:opacity-75">
+              Login
+            </Link>
+            <Link href="/sign-up" className="text-white hover:opacity-75">
+              Sign Up
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 };
